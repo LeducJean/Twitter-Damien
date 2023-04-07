a8545725dacbc9c0097be5b9b0f849593a276ccb
Jean Leduc <jean.leduc80@gmail.com>
2023-04-07 14:53:32 +0200

    setting bdd

--- deleteAllMessages.php
+++ deleteAllMessages.php
@@ -14,7 +14,7 @@ if(isset($_POST["idTaches"])){
                 $ParamAsupprimer =  $ParamAsupprimer.'?,';
             }
             $ParamAsupprimer = substr($ParamAsupprimer, 0, strlen($ParamAsupprimer)-1);
-            $database = new Database("192.168.65.36", "to_do_list", "root", "root");
+            $database = new BDD("192.168.65.164", "connexion", "root", "root");
             $query = "DELETE FROM `tache` WHERE id in (".$ParamAsupprimer.")";
             $PDOstatement = $database->executeQuery($query, $ids );
             if($PDOstatement->rowCount()>0){
