6972c262432c03d3b1f7f4caa574b2a16411398a
Jean <jean.leduc80@gmail.com>
2023-05-11 14:59:17 +0200

    commit bdd ok bon

--- twitter.sql
+++ twitter.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost
--- Généré le : jeu. 11 mai 2023 à 14:53
+-- Généré le : jeu. 11 mai 2023 à 14:59
 -- Version du serveur : 10.5.18-MariaDB-0+deb11u1
 -- Version de PHP : 7.4.33
 
@@ -20,6 +20,19 @@ SET time_zone = "+00:00";
 --
 -- Base de données : `twitter`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `likes`
+--
+
+CREATE TABLE `likes` (
+  `id` int(10) NOT NULL,
+  `user_id` int(10) NOT NULL,
+  `message_id` int(10) NOT NULL,
+  `countLikes` int(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -43,11 +56,43 @@ INSERT INTO `messages` (`id`, `user_id`, `avatar`, `message`, `Date`) VALUES
 (18, 2, NULL, 'damso', '2023-05-11 14:52:07'),
 (19, 1, NULL, 'eaea', '2023-05-11 14:53:16'),
 (20, 1, NULL, 'ytryr', '2023-05-11 14:53:18'),
-(21, 2, NULL, 'fezfez', '2023-05-11 14:53:23');
+(21, 2, NULL, 'fezfez', '2023-05-11 14:53:23'),
+(22, 1, NULL, 'eaea', '2023-05-11 14:55:53'),
+(23, 1, NULL, 'rara', '2023-05-11 14:55:56');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `logname` varchar(50) NOT NULL,
+  `logpass` varchar(50) NOT NULL,
+  `logemail` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `user`
+--
+
+INSERT INTO `user` (`id`, `logname`, `logpass`, `logemail`) VALUES
+(1, 'jean', 'jean', 'jean@gmail.com'),
+(2, 'dams', 'dams', 'dams@gmail.com');
 
 --
 -- Index pour les tables déchargées
 --
+
+--
+-- Index pour la table `likes`
+--
+ALTER TABLE `likes`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `user_id_2` (`user_id`,`message_id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `message_id` (`message_id`);
 
 --
 -- Index pour la table `messages`
@@ -57,18 +102,44 @@ ALTER TABLE `messages`
   ADD KEY `user_id` (`user_id`);
 
 --
+-- Index pour la table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `logname` (`logname`,`logemail`);
+
+--
 -- AUTO_INCREMENT pour les tables déchargées
 --
+
+--
+-- AUTO_INCREMENT pour la table `likes`
+--
+ALTER TABLE `likes`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT pour la table `messages`
 --
 ALTER TABLE `messages`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+
+--
+-- AUTO_INCREMENT pour la table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Contraintes pour les tables déchargées
 --
+
+--
+-- Contraintes pour la table `likes`
+--
+ALTER TABLE `likes`
+  ADD CONSTRAINT `likes_ibfk_1` FOREIGN KEY (`message_id`) REFERENCES `messages` (`id`),
+  ADD CONSTRAINT `likes_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
 
 --
 -- Contraintes pour la table `messages`
